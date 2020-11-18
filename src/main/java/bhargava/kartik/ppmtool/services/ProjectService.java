b3a2e57ac32b84d7bb9ab13b6cbd2c03b823aef6
KartikBhargava <bhargava.kartik07@gmail.com>
2020-11-19 00:37:27 +0530

    Validations part 1 done

---
+++ src/main/java/bhargava/kartik/ppmtool/services/ProjectService.java
@@ -0,0 +1,17 @@
+package bhargava.kartik.ppmtool.services;
+
+import bhargava.kartik.ppmtool.domain.Project;
+import bhargava.kartik.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project) {
+        return projectRepository.save(project);
+    }
+}
